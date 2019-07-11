35f68faf8c123abf3f606e70d597637b2c7985fe
whitehatjr <52084703+whitehatjr@users.noreply.github.com>
2019-07-12 02:46:13 +0530

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,28 @@
+class Log {
+    constructor(x, y, height, angle) {
+      var options = {
+          'restitution':0.8,
+          'friction':1.0,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y, 20, height, options);
+      this.width = 20;
+      this.height = height;
+      Matter.Body.setAngle(this.body, angle);
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      strokeWeight(4);
+      stroke("green");
+      fill(255);
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  };
+  
---
+++ Pig.js
@@ -0,0 +1,27 @@
+class Pig {
+    constructor(x, y) {
+      var options = {
+          'restitution':0.8,
+          'friction':0.3,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y, 50, 50, options);
+      this.width = 50;
+      this.height = 50;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+
+      fill("green");
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  };
+  
